c5b06000c23a0cc39631126e5c99900a02888e9a
GiuntaGiovanni <jhonniest741@gmail.com>
2025-01-14 17:00:11 +0100

    [feat] first general human code

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l’utente deve inserire i numeri che ha visto precedentemente, nell’ordine che preferisce.
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+// salvare le variabili di input
+    // salvare la lista dove aggiungere i li con i numeri che verranno creati 
+    // il pulsante validatura
+
+// salvare le variabili di output
+    // gli input che mostrano i numeri casuali
+    // il paragrafo che mostra il countdown
+
+
+
+
+// funzione di countdown di 30 secondi
+
+// funzione per generare numeri casuali con un min e un max number
+
+
